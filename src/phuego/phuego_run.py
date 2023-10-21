0fe11fe24eba1c46443c5cace3a65ed5871292ba
CharlieBarker <zcbtark@ucl.ac.uk>
2023-10-21 11:41:04 +0100

    for phuego

---
+++ src/phuego/phuego_run.py
@@ -0,0 +1,9 @@
+from phuego import dataprep
+
+support_data_folder = "."
+
+# Download support dataset. If the user wish the keep the zip files for easier 
+# data transfer between different local user/folders, change remove_zip_file to 
+# False.
+
+dataprep(support_data_folder=support_data_folder)
