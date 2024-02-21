a4939aac141c029d4b0c5f8dd7db5d469a59c462
CharlieBarker <zcbtark@ucl.ac.uk>
2024-02-21 12:00:25 +0000

    rtk vis

--- src/mofa/RTK_visualisation.R
+++ src/mofa/RTK_visualisation.R
@@ -5,6 +5,8 @@ library(pathview)
 library(MOFA2)
 library(tidyr)
 library(ggplot2)
+library(dplyr)
+library(ggpubr)
 
 ###enrichment of the weights #### 
 MOFAobject.trained<-load_model(file = "./results/mofa/mofa_object.hdf5")
@@ -42,12 +44,16 @@ RTKs<-c("IGF1R",
         "CD44", 
         "INSR",
         "MMP1")
-neg_feedback<-c("DUSP1", "DUSP2", "DUSP4")
+RTK_figure1 <- c("EGFR", "EGF", "KIT", "FGFR2", "FGF1", "FGFR1", "FGF2")
+neg_feedback_dusp<-c("DUSP1", "DUSP14", "DUSP2",   "DUSP21",  "DUSP4",   "DUSP6")
+neg_feedback_spry<-c("SPRED1", "SPRED2", "SPRY1", "SPRY2", "SPRY4")
+
 #for interest
 for_interest<-c("MMP1", "HLA.DRB1")
 
-
-rtk_mrna<-reshape2::melt(list_of_inputs$mRNA[list_of_inputs$mRNA$X %in% for_interest,])
+all_of_interest <- c(RTK_figure1, neg_feedback_dusp, neg_feedback_spry)
+rtk_mrna<-reshape2::melt(list_of_inputs$mRNA[list_of_inputs$mRNA$X %in% all_of_interest,])
+rtk_mrna$X <- factor(rtk_mrna$X, levels=all_of_interest)
 rtk_mrna$variable<-sub("*\\.[0-9]", "", rtk_mrna$variable)
 rtk_mrna <- rtk_mrna |>
   separate_wider_delim(variable, delim = "__", names = c("drug", "ko"))
@@ -59,15 +65,28 @@ desired_order <- c("Untreated", "Vemurafenib", "Trametinib", "Combination")
 # Convert my_column to a factor with the specified order
 rtk_mrna$drug <- factor(rtk_mrna$drug, levels = desired_order)
 
+output_file<-"./paper/plots/phuego/factor1_rna_abundances.pdf"
+pdf(# The directory you want to save the file in
+  width = 14, # The width of the plot in inches
+  height = 5,
+  file = output_file)
 rtk_mrna %>%
-  ggplot( aes(x=ko, y=value, fill=drug)) +
+  filter(ko == "WT")  %>%
+  ggplot( aes(x=drug, y=value, fill=drug)) +
   geom_boxplot() +
   geom_jitter(color="black", size=0.4, alpha=0.9) +
   scale_fill_manual(values = drug_colors) +
+  cowplot::theme_cowplot() +
   theme(
-    legend.position="none",
-    plot.title = element_text(size=11)
+    plot.title = element_text(size=11),
+    axis.text.x = element_text(angle = 70, hjust = 1, size = rel(1)),
   ) +
-  ggtitle("mRNA abundances of central RTKs") +
-  xlab("") + cowplot::theme_cowplot() +
-  facet_wrap(~X, scales = "free_y")
+  xlab("") +
+  facet_wrap(~X, scales = "free_y", nrow = 2) + 
+  grids(linetype = "dashed")+
+  labs(
+    x = "Drug treatment",
+    y = "Voom-normalised RNA expression",
+  )
+dev.off()
+  
