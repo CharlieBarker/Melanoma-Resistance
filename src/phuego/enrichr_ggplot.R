ac4a3680670aa9e45112976bbc068a878dcc4539
CharlieBarker <zcbtark@ucl.ac.uk>
2024-09-24 15:54:37 +0100

    enrichr

--- src/phuego/enrichr_ggplot.R
+++ src/phuego/enrichr_ggplot.R
@@ -1,5 +1,5 @@
 
-#find correct environment 
+#find correct environment
 packLib="/usr/lib/R"
 if (file.exists(packLib)) {
   reticulate::use_condaenv("py37", required = T)
@@ -12,19 +12,30 @@ if (file.exists(packLib)) {
 library(ggplot2)
 library(ggrepel)
 
+log_pval_cutoff<-20
+odds_ratio_cutoff<-60
 
-enrichr_bioplanet2019<-read.csv(file = "./results/phuego/enrichr_dfs/factor1.csv")
+enrichr_bioplanet2019<-read.csv(file = "./results/phuego/enrichr_dfs/factor3and1.csv")
 enrichr_bioplanet2019$label<-
 enrichr_bioplanet2019$label<-""
-enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>10]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$log_pval>10]
-enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>50]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>50]
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>log_pval_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$log_pval>log_pval_cutoff]
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]
 
-pdf(file = paste0("/Users/charliebarker/Desktop/Melanoma_Resistance/paper/for_sumana/factor1/enrichr_enrichment.pdf"), 
+pdf(file = paste0("/Users/charliebarker/Desktop/Melanoma_Resistance/paper/for_sumana/factor3/enrichr_enrichment_factor3and1.pdf"),
     width = 8, height = 8)
 # basic scatterplot
-ggplot(enrichr_bioplanet2019, aes(x=odds_ratio, y=log_pval, label=label)) + 
+ggplot(enrichr_bioplanet2019, aes(x=odds_ratio, y=log_pval, label=label)) +
   geom_point() +
-  ggrepel::geom_text_repel(size = 2) + cowplot::theme_cowplot()
+  ggrepel::geom_text_repel(size = 4) +
+  cowplot::theme_cowplot() +
+  theme(
+    plot.title = element_text(size = 15, face = "bold"),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 1),
+  ) +
+  grids(linetype = "dashed") +
+  xlab(paste("Odd Ratio")) +
+  ylab("Log10(adjusted P value)") +
+  ggtitle("Enrichment of terms in combined ARID1A/drug treatment network")
 dev.off()
 
-#check the immune in the combination. 
+#check the immune in the combination.
