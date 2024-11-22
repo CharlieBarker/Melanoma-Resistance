c95029fa07cb1dd05e2be474e54b7237dda55258
CharlieBarker <zcbtark@ucl.ac.uk>
2024-11-22 15:09:29 +0100

    enrichments

--- src/phuego/enrichr_ggplot.R
+++ src/phuego/enrichr_ggplot.R
@@ -10,6 +10,8 @@ if (file.exists(packLib)) {
 
 
 library(ggplot2)
+library(ggpubr)
+library(dplyr)
 library(ggrepel)
 
 log_pval_cutoff<-20
@@ -54,13 +56,13 @@ enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>log_pval_cutoff]<-enr
 enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]
 
 pdf(file = paste0("/Users/charliebarker/Desktop/Melanoma_Resistance/paper/Supplementary_plots/enrichr_enrichment_factor2.pdf"),
-    width = 10, height = 16)
+    width = 16, height = 6)
 # basic scatterplot
 ggplot(enrichr_bioplanet2019, aes(x=odds_ratio, y=log_pval, label=label)) +
   geom_point() +
   ggrepel::geom_text_repel(size = 4) +
   cowplot::theme_cowplot() +
-  facet_wrap(~direction, ncol = 1) +
+  facet_wrap(~direction, ncol = 2) +
   theme(
     plot.title = element_text(size = 15, face = "bold"),
     panel.border = element_rect(colour = "black", fill = NA, linewidth = 1),
@@ -72,4 +74,87 @@ ggplot(enrichr_bioplanet2019, aes(x=odds_ratio, y=log_pval, label=label)) +
 dev.off()
 
 #check the immune in the combination.
+
+
+log_pval_cutoff<-10
+odds_ratio_cutoff<-35
+
+enrichr_bioplanet2019<-read.csv(file = "./results/phuego/enrichr_dfs/factor2_down.csv")
+enrichr_bioplanet2019$label<-""
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>log_pval_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$log_pval>log_pval_cutoff]
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]
+
+pdf(file = paste0("/Users/charliebarker/Desktop/Melanoma_Resistance/paper/Supplementary_plots/enrichr_enrichment_factor2.pdf"),
+    width = 10, height = 8)
+# Basic scatterplot with improved ggrepel
+ggplot(enrichr_bioplanet2019, aes(x = odds_ratio, y = log_pval, label = label)) +
+  geom_point() +
+  ggrepel::geom_text_repel(
+    min.segment.length = 0,
+    size = 5,
+    segment.color = "black",  # Ensures lines are drawn to points
+    segment.size = 0.5        # Adjust line thickness
+  ) +
+  cowplot::theme_cowplot() +
+  theme(
+    plot.title = element_text(size = 18, face = "bold"),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 1)
+  ) +
+  grids(linetype = "dashed") +
+  xlab("Odd Ratio") +
+  ylab("Log10(adjusted P value)") +
+  ggtitle("Enrichment of terms in downregulated combination-therapy network")
+
+enrichr_bioplanet2019<-read.csv(file = "./results/phuego/enrichr_dfs/factor2_up.csv")
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>log_pval_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$log_pval>log_pval_cutoff]
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]
+
+# Basic scatterplot with improved ggrepel
+ggplot(enrichr_bioplanet2019, aes(x = odds_ratio, y = log_pval, label = label)) +
+  geom_point() +
+  ggrepel::geom_text_repel(
+    min.segment.length = 0,
+    size = 5,
+    segment.color = "black",  # Ensures lines are drawn to points
+    segment.size = 0.5        # Adjust line thickness
+  ) +
+  cowplot::theme_cowplot() +
+  theme(
+    plot.title = element_text(size = 18, face = "bold"),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 1)
+  ) +
+  grids(linetype = "dashed") +
+  xlab("Odd Ratio") +
+  ylab("Log10(adjusted P value)") +
+  ggtitle("Enrichment of terms in upregulated combination-therapy network")
+
+dev.off()
+
+pdf(file = paste0("/Users/charliebarker/Desktop/Melanoma_Resistance/paper/Supplementary_plots/enrichr_enrichment_factor1.pdf"),
+    width = 6)
+
+enrichr_bioplanet2019<-read.csv(file = "./results/phuego/enrichr_dfs/factor1.csv")
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$log_pval>log_pval_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$log_pval>log_pval_cutoff]
+enrichr_bioplanet2019$label[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]<-enrichr_bioplanet2019$term[enrichr_bioplanet2019$odds_ratio>odds_ratio_cutoff]
+
+# Basic scatterplot with improved ggrepel
+ggplot(enrichr_bioplanet2019, aes(x = odds_ratio, y = log_pval, label = label)) +
+  geom_point() +
+  ggrepel::geom_text_repel(
+    min.segment.length = 0,
+    size = 3,
+    segment.color = "black",  # Ensures lines are drawn to points
+    segment.size = 0.5        # Adjust line thickness
+  ) +
+  cowplot::theme_cowplot() +
+  theme(
+    plot.title = element_text(size = 18, face = "bold"),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 1)
+  ) +
+  grids(linetype = "dashed") +
+  xlab("Odd Ratio") +
+  ylab("Log10(adjusted P value)") +
+  ggtitle("Enrichment of terms in combined drug-agnostic network")
+
+dev.off()
 
